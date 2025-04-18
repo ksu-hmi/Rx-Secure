f72b3981db3bdc84cb6b307187c41a9266339047
pravinya12 <pmandal2@students.kennesaw.edu>
2025-04-18 15:27:32 -0400

    Update config.py
    added code

--- config.py
+++ config.py
@@ -1,1 +1,26 @@
+from os import environ, path
+from dotenv import load_dotenv
+import requests
 
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+class Config:
+    """Set Flask configuaration from .env file"""
+
+    # general config
+    try:
+        FLASK_APP = environ.get('FLASK_APP')
+        FLASK_ENV = environ.get('FLASK_ENV')    
+    except:
+        pass
+
+    # using microservice to set unique secret key
+    r = requests.get('https://uuid-genie.herokuapp.com/api/uuid')
+    uuid = r.json()
+    SECRET_KEY = uuid['uuid']
+    
+    # static Assets
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    COMPRESSOR_DEBUG = True
