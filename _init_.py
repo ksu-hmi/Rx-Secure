a063438765e948851f035473e075089d11410a59
pravinya12 <pmandal2@students.kennesaw.edu>
2025-04-21 17:51:20 -0400

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from config import Config
 
+
+def init_app():
+    """Initialize the core app"""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        from . import routes
+
+        return app
